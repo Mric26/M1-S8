2340237ffc55085cc56e4ade200e66c840b90bc7
Chevailler <gillian.chevailler@gmail.com>
2015-03-07 16:12:24 +0100

    ajout de zoom/dézoom a 5% dans le tp3 de TLI

--- TLI/TP2/grapher/src/grapher/ui/EcouteurDeSouris.java
+++ TLI/TP2/grapher/src/grapher/ui/EcouteurDeSouris.java
@@ -2,17 +2,28 @@ package grapher.ui;
 
 import javax.swing.event.MouseInputAdapter;
 
+import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
 
 class EcouteurDeSouris extends MouseInputAdapter implements MouseWheelListener {
 	public Grapher graph;
 	int etat;
 	int posx;
 	int posy;
+	boolean zoom= false;
 
     EcouteurDeSouris(Grapher g) {
     	graph = g;
@@ -52,6 +63,7 @@ class EcouteurDeSouris extends MouseInputAdapter implements MouseWheelListener {
     		graph.zoom(p1, e.getY()-posy );
     	}
     	else if ( etat == 3){
+    		zoom= true;
     	}
     	else{
     		System.out.println("Autre !!!");
@@ -63,10 +75,30 @@ class EcouteurDeSouris extends MouseInputAdapter implements MouseWheelListener {
     }
     
     public void mouseReleased(MouseEvent e) {
-    	if(etat == 3){
-    		Point p1 = new Point(posx,posy);
-    		Point p2 = new Point(e.getX(),e.getY());
+    	if(etat == 1){ //zoom de 5%
+    		Dimension d= graph.getPreferredSize();
+    		int x, y;
+    		x= (int)((d.width -d.width*0.05)/2);
+    		y= (int)((d.height -d.height*0.05)/2);
+    		Point p1 = new Point(e.getX() -x,e.getY() -y);
+    		Point p2 = new Point(e.getX() +x,e.getY() +y);
     		graph.zoom(p1, p2);
+    	}else if(etat == 3){
+    		if (zoom){ //zoom selon une région rectangulaire
+    			Point p1 = new Point(posx,posy);
+        		Point p2 = new Point(e.getX(),e.getY());
+        		graph.zoom(p1, p2);
+        		zoom = false;
+    		}else{ //dézoom de 5%
+    			Dimension d= graph.getPreferredSize();
+        		int x, y;
+        		x= (int)((d.width +d.width*0.05)/2);
+        		y= (int)((d.height +d.height*0.05)/2);
+        		Point p1 = new Point(e.getX() -x,e.getY() -y);
+        		Point p2 = new Point(e.getX() +x,e.getY() +y);
+        		graph.zoom(p1, p2);
+    		}
+    		
     	}
     	graph.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
     }
