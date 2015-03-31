99db3e4eb487dc7951545e92175dfc469b9a846d
segureta <segureta@F213-02.e-im2ag.ujf-grenoble.fr>
2015-03-31 10:01:41 +0200

    yolo swag

--- TLI/TP2/grapher/src/grapher/ui/Main.java
+++ TLI/TP2/grapher/src/grapher/ui/Main.java
@@ -4,13 +4,21 @@ import grapher.fc.Function;
 import grapher.fc.FunctionFactory;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.MenuBar;
+import java.awt.event.KeyEvent;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
 import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 
@@ -37,6 +45,18 @@ public class Main extends JFrame {
 	    EcouteurDeListe ecdl = new EcouteurDeListe( list, grapher );
 	    list.addListSelectionListener(ecdl);
 	    
+	    //table
+//	    Function[][] donnees = new Function[1][functions.size()];
+//	    for (int i = 0; i < functions.size(); i++) {
+//			donnees[i][0] = functions.elementAt(i);
+//		}
+//	    JColorChooser[][] couleur = new JColorChooser[1][functions.size()];
+//	    for (int i = 0; i < functions.size(); i++) {
+//	    	couleur[i][0] = new JColorChooser();
+//		}
+//        String[] entetes = {"Fonctions", "Couleur"};
+//        JTable tableau = new JTable(donnees, couleur);
+	    
 	    //toolbar ajout/supp
 	    JButton b1 = new JButton("+");
 	    b1.addActionListener( new AjouterFonctionListe(functions, grapher) );
@@ -57,11 +77,23 @@ public class Main extends JFrame {
 	    JSplitPane jp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 		jp.setOneTouchExpandable(true);
 		jp.setDividerLocation(100);
+		
+		//barre de menu
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("Expression");
+		menuBar.add(menu);
+		JMenuItem menuItemAdd = new JMenuItem("Add");
+		menuItemAdd.addActionListener( new AjouterFonctionListe(functions, grapher) );
+		JMenuItem menuItemRemove = new JMenuItem("Remove");
+		menuItemRemove.addActionListener( new SupprimerFonctionListe(functions, list, grapher) );
+		menu.add(menuItemAdd);
+		menu.add(menuItemRemove);
 	    
 		//Ajouts
 		jp.add(panel);
 		jp.add(grapher);
 		add(jp);
+		this.setJMenuBar(menuBar);
 		pack();
 	}
 
